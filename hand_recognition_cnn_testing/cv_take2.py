547204863dbd0a900251ead8565b61a0c3b84e40
arjunm8 <arjunm1196@gmail.com>
2018-04-19 15:36:04 +0530

    few trivial updates

--- hand_recognition_cnn_testing/cv_take2.py
+++ hand_recognition_cnn_testing/cv_take2.py
@@ -5,62 +5,252 @@ Spyder Editor
 This is a temporary script file.
 """
 
+#29 15 40
+#145 130 158
+
 import cv2
 import numpy as np
 import time
+import pyautogui
+import test1
+from win32api import keybd_event
+
+
+
+def press(Key, speed=1):
+    rest_time = 0.05/speed
+    #keydown
+    keybd_event(Key, 0, 1, 0)
+    time.sleep(rest_time)
+    #keyup
+    keybd_event(Key, 0, 2, 0)
+ 
+
+def nothing(x):
+    pass
 
 cap = cv2.VideoCapture(0)
+frame = cap.read()[1]
+cv2.namedWindow('bars')
+
+cv2.createTrackbar('lH','bars',0,255,nothing)
+cv2.createTrackbar('lS','bars',0,255,nothing)
+cv2.createTrackbar('lV','bars',0,255,nothing)
+
+cv2.createTrackbar('uH','bars',0,255,nothing)
+cv2.createTrackbar('uS','bars',0,255,nothing)
+cv2.createTrackbar('uV','bars',0,255,nothing)
+
+cv2.setTrackbarPos('lH','bars',0)
+cv2.setTrackbarPos('lS','bars',0)
+cv2.setTrackbarPos('lV','bars',0)
+cv2.setTrackbarPos('uH','bars',255)
+cv2.setTrackbarPos('uS','bars',255)
+cv2.setTrackbarPos('uV','bars',255)
+
 
 count = 0
 active = False
+bg = None
+trimbg = False
+mouse_slow = False
+new_center = None
+pyautogui.FAILSAFE = False
+screen_cam_ratioxy = None
+brains = True
+keyT_or_mouseF = False
+         
+d_thresh = 30 if(mouse_slow) else 3    
+m_thresh = 0.3 if(mouse_slow) else 2.2
+
+inference = None
+lastI = None
+
+if(brains):
+    test1.load_weights()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+
 while True:
     #time.sleep(0.1)
     frame = cap.read()[1]
+    frame = cv2.flip(frame,1)
+
+    lH = cv2.getTrackbarPos('lH','bars')
+    lS = cv2.getTrackbarPos('lS','bars')
+    lV = cv2.getTrackbarPos('lV','bars')
     
+    uH = cv2.getTrackbarPos('uH','bars')
+    uS = cv2.getTrackbarPos('uS','bars')
+    uV = cv2.getTrackbarPos('uV','bars')
     
-    #lower = np.array([100, 48, 80], dtype = "uint8")
-    #upper = np.array([200, 255, 255], dtype = "uint8")
-    #mask = cv2.inRange(frame,lower,upper)
+    lower = np.array([lH, lS, lV], dtype = "uint8")
+    upper = np.array([uH, uS, uV], dtype = "uint8")
+    
+   
+    mask = cv2.inRange(frame,lower,upper)
     #if it's in the mask range itll be one else 0
-    #frame = cv2.bitwise_and(frame,frame,mask=mask)
+    frame = cv2.bitwise_and(frame,frame,mask=mask)
 
     
     #coz initial recording orientation was sideways and autistic
     #frame =  cv2.transpose(frame).
-    #frame = cv2.flip(frame,0)
     
-    #crop
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    #Apply skin color range
-    mask = cv2.inRange(hsv, low_range, upper_range)
+    #smoothing/averaging
+    #divide by 15x15=225,(if (5,5) then 25)
+    #kernel = np.ones((15,15),np.float32)/225
+    #frame= cv2.filter2D(frame,-1,kernel)
+    
+    
 
-    mask = cv2.erode(mask, skinkernel, iterations = 1)
-    mask = cv2.dilate(mask, skinkernel, iterations = 1)
+    #blurring
+    #blur = cv2.GaussianBlur(frame,(15,15),0)
+
+    #erosion and dilation(expanding,squeezing pixel distributions)
+    #kernel = np.ones((5,5),np.uint8)
+    #erosion = cv2.erode(frame, kernel, iterations=1)
+    #frame = cv2.dilate(erosion, kernel, iterations=1)
+
+
+    #opening/ remove false positives, closing/remove false negatives
+    #opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN,kernel)
+    #frame = cv2.morphologyEx(opening, cv2.MORPH_CLOSE,kernel)
+        
+    #256x256 crop
+    x = frame[:256,384:]
+
+
+    if(trimbg):
+        x = cv2.absdiff(bg,x)
+        
+        mask = cv2.inRange(x,lower,upper)
+        #if it's in the mask range itll be one else 0
+        x = cv2.bitwise_and(x,x,mask=mask)
+
+        
+        # color to grayscale
+        x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
     
-    #blur
-    mask = cv2.GaussianBlur(mask, (15,15), 1)
-    #cv2.imshow("Blur", mask)
+        ret2, x= cv2.threshold(x,20,255,cv2.THRESH_BINARY)
+        #256x256 crop
+        #x = frame[:256,384:]
+        #top, left, right, bottom
+        x[:2,:] = x[:,:2] = x[:,-2:] =  x[-2:,:] = 0
+        
+        if(brains):
+            inference = test1.get(cv2.resize(x,(64,64)).reshape((64,64,1))/255)
+            cv2.putText(frame,inference,(0,50),font,2,(255,255,255),2,cv2.LINE_AA)
+        if(keyT_or_mouseF):
+            if(inference=='index'):
+                #right
+                press(32)  
+            '''
+            elif(inference=='index'):
+                #right
+                press(32)
+            elif(inference=='index'):
+                #right
+                press(32)
+            elif(inference=='index'):
+                #right
+                press(32)
+            elif(inference=='index'):
+                #right
+                press(32)
+            '''
+            
+        if(not keyT_or_mouseF):
+            i, contours, heirarchy = cv2.findContours(x, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+            cont_sorted = sorted(contours, key=cv2.contourArea, reverse=True)[:5] if (not keyT_or_mouseF) else None
+            exists = False
+            
+            #remember x row count is the height and column count is the width 
+            
+            if(cont_sorted):
+                for i in range(len(cont_sorted)):
+                    contrec = cv2.boundingRect(cont_sorted[i])
+                    if 50 < contrec[2] < 250:
+                        x1,y1,w,h = contrec
+                        h = w
+                        cv2.rectangle(x,(x1,y1),(x1+w,y1+ h ),(255,255,255),1)
+                        exists = True
+                        break
+                center = (x.shape[1]//2,int(x.shape[0]/2*1.2))
+                cv2.circle(x,center,4,[255,255,255],2)
+                cv2.circle(x,center,1,[0,0,0],2)
+                if(exists):
+                        if (not new_center):
+                            new_center = center
+                        #screenwidth/camwidth, scheight/camheight
+                        if(not screen_cam_ratioxy):
+                            screen_cam_ratioxy = (pyautogui.size()[0]/x.shape[1],pyautogui.size()[1]/x.shape[0])
+                            
+                        hcenter = (x1+(w//2),y1+(h//2))
+                        cv2.circle(x,hcenter,2,[0,0,0],1)
+                        # sqrt( (x2-x1)^2 + (y2-y1)^2 )
+                        d = np.sqrt( (new_center[0]-hcenter[0])**2 + (new_center[1]-hcenter[1])**2 )  
+                        #print("distance: ",d)
+                        if(d>d_thresh and inference=='Fist'):
+                            
+                            if(d<5):
+                                m_thresh = 0.5
+                            elif(d<12):
+                                m_thresh = 1
+                            elif(d<15):
+                                m_thresh = 2
+                            elif(d<18):
+                                m_thresh = 3
+                            elif(d>18):
+                                m_thresh = 4
+                            print(d)
+                            
+                            
+                            nx = int((hcenter[0]-new_center[0])*screen_cam_ratioxy[0] * m_thresh)
+                            ny = int((hcenter[1]-new_center[1])*screen_cam_ratioxy[1] * m_thresh)
+                            pyautogui.moveRel(nx,ny)
+                            if(not mouse_slow):
+                                new_center = hcenter
+                        elif(inference=='index' and lastI!='index'):
+                            pyautogui.click()
+                        elif(inference=='peace' and lastI!='peace'):
+                            pyautogui.doubleClick()
+            lastI = inference
+
+    else:
+        x = frame[:256,384:]
     
-    #bitwise and mask original frame
-    res = cv2.bitwise_and(roi, roi, mask = mask)
-    # color to grayscale
-    res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    y = x
     
+    cv2.rectangle(frame, (639,0),(384,256),(255,255,255),2)
+    #x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+    x = cv2.resize(x,(64,64))
+
+
+    cv2.imshow('image',frame)
+    cv2.imshow('x',x)
     
+    if cv2.waitKey(1) & 0xFF == ord('b'):
+        if(trimbg):
+            trimbg = False
+        else:
+            bg = y
+            print("bg cap")
+            trimbg = True
     
-    frame = frame[:480,:480]
-    
-    
-    cv2.imshow('original',res)
-    #frame = cv2.resize(frame,(128,128))
+    if cv2.waitKey(1) & 0xFF == ord('m'):
+        keyT_or_mouseF = False if(keyT_or_mouseF) else True
+            
     if cv2.waitKey(1) & 0xFF == ord('f'):
         active = False if active else True
     if active:
-        cv2.imwrite("fresh_data/five"+str(count)+".jpg",frame)
+        cv2.imwrite("data/train/null/null_"+str(count)+".jpg",x)
         print("cap count:",count)
         count+=1
+        
     if cv2.waitKey(2) & 0xFF == ord(' '):
         break
 cap.release()
 cv2.destroyAllWindows()
+  
--- hand_recognition_cnn_testing/test1.py
+++ hand_recognition_cnn_testing/test1.py
@@ -14,15 +14,39 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, Activation, MaxPooling2D, Dropout, Flatten, Dense
 import cv2
-import time
 
-TRAINING_MODE = False
+'''
+TRAINING_MODE = 1
+RUN_MODE = 2
+TRAIN_CHKPT = 3
+EVAL = 4
+init funcs = 0
+'''
+MODE = 0
+
+p=None
+
 BENCHMARK = False
+states = ["Fist",
+          "Five",
+          "index",
+          "md",
+          "null",
+          "peace"
+         ]
 
 model = Sequential()
-model.add(  Conv2D(32,(3,3),padding='same',input_shape=(64,64,3)) )
+model.add(  Conv2D(16,(3,3),padding='same',input_shape=(64,64,1)) )
 model.add(Activation('relu'))
-model.add(  Conv2D(32,(3,3),padding='same') )
+model.add(  Conv2D(16,(3,3),padding='same') )
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.10))
+
+model.add(Conv2D(32,(3,3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(32,(3,3), padding='same'))
 model.add(Activation('relu'))
 
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -39,31 +63,33 @@ model.add(Dropout(0.10))
 model.add(Flatten())
 model.add(Dense(256))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.1))
 
 model.add(Dense(256))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.1))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(6))
+model.add(Activation('softmax'))
+
+
 
 
 #custom_optimizer = keras.optimizers.rmsprop(lr = 0.001,decay=1e-4)
 model.compile(
-            loss='binary_crossentropy',
+            loss='categorical_crossentropy',
             optimizer='rmsprop',
             metrics=['accuracy']
         )
 
 
-batch_size = 32
+batch_size = 128
 
 #https://keras.io/preprocessing/image/
 datagen = ImageDataGenerator(
-        rescale=1./255,#*
+        rescale=1.0/255,#*
         #shear_range=0.2,#random shear transformations (google)
-        rotation_range=120, # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=60, # randomly rotate images in the range (degrees, 0 to 180)
         width_shift_range=0.2, # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.2, # randomly shift images vertically (fraction of total height)
         horizontal_flip=True, # randomly flip images
@@ -72,43 +98,90 @@ datagen = ImageDataGenerator(
 
 training_gen = datagen.flow_from_directory(
                         'data/train',
+                        color_mode='grayscale',
                         target_size=(64,64),
                         batch_size = batch_size,
                         #using binary_crossentropy loss function so we need binary labels
-                        class_mode='binary'
+                        class_mode="categorical",
+                        shuffle=True
                     )
 
 validation_gen = ImageDataGenerator(
-                        rescale=1./255
+                        rescale=1.0/255
                     ).flow_from_directory(
                         'data/validation',
+                        color_mode='grayscale',
                         target_size=(64,64),
                         batch_size = batch_size,
-                        class_mode='binary'           
+                        class_mode='categorical', 
+                        shuffle=True
                     )
 
+def load_weights():
+    #global model
+    #model = keras.models.load_model("newMultiM_001.h5")
+    #print("model loaded")
+    model.load_weights("newMultiW_001.h5")
+    print("weights loaded")
+
+def save_weights():
+    # Save model and weights
+    model.save("newMultiM_001.h5")
+    model.save_weights("newMultiW_001.h5")
+    print('Saved trained model and weights')
+
+
+def train():
+    model.fit_generator(
+                training_gen,
+                steps_per_epoch=training_gen.samples // batch_size,
+                epochs=20,
+                validation_data=validation_gen,
+                validation_steps=validation_gen.samples // batch_size
+            )
+    eval()
+    save_weights()
+
+def eval():
+    scores = model.evaluate_generator(
+            validation_gen,
+            steps=validation_gen.samples // batch_size
+        )
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
 
 def get(x):
+    global p
     p = model.predict(x.reshape((1,) + x.shape))
-    #p = np.argmax(p)
-    print(p)
-    if p==[[ 0.]]:
-        return "Fist"
-    elif p==[[ 1.]]:
-        return "Five"
-    #else:
-    #    return "null"
+    q = np.argmax(p)
+    print(p*100//1)
+    return states[q]
 
-def cap():
+frame = []
+
+def start_cap():
+    global frame
     cap = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
+    c=0
+    print("camera loaded")
     while True:
         frame = cap.read()[1]
-        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        x = cv2.resize(frame,(64,64))
-        inference = get(x)
+        frame = cv2.flip(frame,1)
+        x = frame[:256,384:]
+        cv2.rectangle(frame, (639,0),(384,256),(0,255,0),2)
+        #x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+        x = cv2.resize(x,(64,64))
+        if(c):
+            print("camera init complete")
+        #Don't ever forget normalization idiot
+        inference = get(x/255)
+        if(c):
+            print("inference functional")
+            c=1
         cv2.putText(frame,inference,(0,50),font,2,(255,255,255),2,cv2.LINE_AA)        
         cv2.imshow('original',frame)
+        cv2.imshow('read',x)
         if cv2.waitKey(1) & 0xFF == ord(' '):
             break
     cap.release()
@@ -119,26 +192,32 @@ def k():
     cap.release()
     cv2.destroyAllWindows()
 
-if TRAINING_MODE:
-    model.fit_generator(
-                training_gen,
-                steps_per_epoch=200,
-                epochs=10,
-                validation_data=validation_gen,
-                validation_steps=100
-            )
     
-    # Save model and weights
-    model.save("lg_deep_saved_model_001.h5")
-    model.save_weights("lg_deep_saved_weights_001.h5")
-    print('Saved trained model and weights')
-else:
-    model = keras.models.load_model("deep_saved_model_002.h5")
-    model.load_weights("deep_saved_weights_002.h5")
-    cap()
+def load_and_train():
+    load_weights()
+    train()
     
+def load_and_cap():
+    load_weights()
+    start_cap()
+
+if __name__ == '__main__':
+    if MODE==1:
+        train()
+    elif MODE==2:
+        load_and_cap()
+    elif MODE==3:
+        load_and_train()
+    elif MODE==4:
+        eval()
+    
+       
 
 
 '''
-*rescale is a value by which we will multiply the data before any other processing. Our original images consist in RGB coefficients in the 0-255, but such values would be too high for our models to process (given a typical learning rate), so we target values between 0 and 1 instead by scaling with a 1/255. factor.
+*rescale is a value by which we will multiply the data before any other
+ processing. Our original images consist in RGB coefficients in the 0-255,
+ but such values would be too high for our models to process (given a typical
+ learning rate), so we target values between 0 and 1 instead by scaling with
+ a 1/255. factor.
 '''
